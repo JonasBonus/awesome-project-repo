b7b253e6b278156d1a4d69a4903e06f041b69662
Jonas <jonasbennettgraff@gmail.com>
2023-06-13 20:09:07 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is jonas's website</h1>
+      <a>(it's not done yet. come back soon)</a>
+    </div>
+  );
+}
+
+export default App;
